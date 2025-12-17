e0476bc13b56ddfb5ada2a86e8538fbaf0d8c640
GROGUEUR <francoislepine72@gmail.com>
2025-12-17 09:42:38 +0100

    SUPPRESSION

--- automl/__init__.p
+++
@@ -1,8 +0,0 @@
-"""
-Package AutoML - Challenge Machine Learning
-"""
-
-from .automl import AutoML
-
-__version__ = "0.1.0"
-__all__ = ['AutoML']
--- test_automl.py
+++
@@ -1,9 +0,0 @@
-from automl import AutoML
-
-# Chemin vers les données du projet
-data_path = "/info/corpus/ChallengeMachineLearning"
-
-# Utiliser votre AutoML
-model = AutoML()
-model.fit(data_path)
-model.eval()
