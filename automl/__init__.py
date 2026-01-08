c08bf416e33816057eaec2d33a90420c3dd0676d
Sahmmm <153719477+Sahmmm@users.noreply.github.com>
2026-01-09 00:30:09 +0100

    Update __init__.py

--- automl/__init__.py
+++ automl/__init__.py
@@ -9,7 +9,7 @@ Usage:
     >>> automl.fit(data_path="/path/to/data")
     >>> automl.eval()
 """
-from .core import fit, eval, get_data, reset
+from .core import fit, eval, get_data, reset, get_trained_models
 
 __version__ = '0.1.0'
-__all__ = ['fit', 'eval', 'get_data', 'reset']
+__all__ = ['fit', 'eval', 'get_data', 'reset','get_trained_models']
